1a48f8c1bb739157d0f73c01a7114347b1bd94e9
ishubham1995 <shubham.malik@crowdbotics.com>
2023-07-21 22:15:23 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "xswer-3",
   url: "https://xswer-3.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- store/xswerAPI/api.js
+++ store/xswerAPI/api.js
@@ -1,68 +1,96 @@
-import axios from "axios"
+import axios from "axios";
 const xswerAPI = axios.create({
   baseURL: "https://xswer-3.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
-  return xswerAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+  return xswerAPI.get(`/api-docs/schema/`, {
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return xswerAPI.post(`/api/v1/login/`, payload)
+  return xswerAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return xswerAPI.post(`/api/v1/signup/`, payload)
+  return xswerAPI.post(`/api/v1/signup/`, payload);
 }
+
 function api_v1_xcds_list(payload) {
-  return xswerAPI.get(`/api/v1/xcds/`)
+  return xswerAPI.get(`/api/v1/xcds/`);
 }
+
 function api_v1_xcds_create(payload) {
-  return xswerAPI.post(`/api/v1/xcds/`, payload)
+  return xswerAPI.post(`/api/v1/xcds/`, payload);
 }
+
 function api_v1_xcds_retrieve(payload) {
-  return xswerAPI.get(`/api/v1/xcds/${payload.id}/`)
+  return xswerAPI.get(`/api/v1/xcds/${payload.id}/`);
 }
+
 function api_v1_xcds_update(payload) {
-  return xswerAPI.put(`/api/v1/xcds/${payload.id}/`, payload)
+  return xswerAPI.put(`/api/v1/xcds/${payload.id}/`, payload);
 }
+
 function api_v1_xcds_partial_update(payload) {
-  return xswerAPI.patch(`/api/v1/xcds/${payload.id}/`, payload)
+  return xswerAPI.patch(`/api/v1/xcds/${payload.id}/`, payload);
 }
+
 function api_v1_xcds_destroy(payload) {
-  return xswerAPI.delete(`/api/v1/xcds/${payload.id}/`)
+  return xswerAPI.delete(`/api/v1/xcds/${payload.id}/`);
 }
+
 function rest_auth_login_create(payload) {
-  return xswerAPI.post(`/rest-auth/login/`, payload)
+  return xswerAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return xswerAPI.get(`/rest-auth/logout/`)
+  return xswerAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return xswerAPI.post(`/rest-auth/logout/`)
+  return xswerAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return xswerAPI.post(`/rest-auth/password/change/`, payload)
+  return xswerAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return xswerAPI.post(`/rest-auth/password/reset/`, payload)
+  return xswerAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return xswerAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return xswerAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return xswerAPI.post(`/rest-auth/registration/`, payload)
+  return xswerAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return xswerAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return xswerAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return xswerAPI.get(`/rest-auth/user/`)
+  return xswerAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return xswerAPI.put(`/rest-auth/user/`, payload)
+  return xswerAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return xswerAPI.patch(`/rest-auth/user/`, payload)
+  return xswerAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -84,4 +112,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
